a82a16a26103bdf9d1edf5e4f2b6150b7fc3cdd5
subham saurabh <saurabhsubham113@gmail.com>
2020-06-29 19:59:43 +0530

    Updates

---
+++ precache-manifest.37384c27e40cd1b9523828e5196cdec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0a454638e9ea6c7f9482ae32b80833",
+    "url": "/joke-app/index.html"
+  },
+  {
+    "revision": "a3205e38742ccffd0883",
+    "url": "/joke-app/static/css/main.9a718d31.chunk.css"
+  },
+  {
+    "revision": "90efba2a594097662f8d",
+    "url": "/joke-app/static/js/2.c4fcbb5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joke-app/static/js/2.c4fcbb5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3205e38742ccffd0883",
+    "url": "/joke-app/static/js/main.6fc005df.chunk.js"
+  },
+  {
+    "revision": "4374efcc382e6f110c78",
+    "url": "/joke-app/static/js/runtime-main.bb6ebe5e.js"
+  }
+]);
